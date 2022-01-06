5775659a4e7adc623a0b34735fdbdb2426dd535a
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2022-01-06 14:03:59 -0600

    Made some tweaks to the whichkey bindings to work better with vim-fugitive

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -87,6 +87,8 @@ local mappings = {
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
   ["w"] = { "<cmd>w!<CR>", "Save" },
   ["q"] = { "<cmd>q!<CR>", "Quit" },
+  ["r"] = { "<cmd>so %<CR>", "Reload nvim file" },
+  ["R"] = { "<cmd>so ~/.config/nvim/init.lua<CR>", "Reload nvim config" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
@@ -108,10 +110,14 @@ local mappings = {
   g = {
     name = "Git",
     g = { "<cmd>G<CR>", "Git Fugitive Status" },
+    f = { "<cmd>G fetch<CR>", "Git fetch" },
+    p = { "<cmd>G pull<CR>", "Git pull" },
+    c = { "<cmd>G commit<CR>", "Git commit" },
+    P = { "<cmd>G push<CR>", "Git push" },
     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    v = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
@@ -121,7 +127,7 @@ local mappings = {
     },
     o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
     b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+    C = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
     d = {
       "<cmd>Gitsigns diffthis HEAD<cr>",
       "Diff",
