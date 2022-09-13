3cb4860b9ead5b8cc6052a0a598834c57ad871c1
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2022-09-13 16:26:14 -0500

    Fixed up the config for the Tokyonight plugin.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,15 +1,19 @@
 local colorscheme = "tokyonight"
 
--- Example config in Lua
--- vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_transparent = false
-vim.g.tokyonight_italic_keywords = false
-vim.g.tokyonight_italic_functions = false
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+require("tokyonight").setup({
+  -- use the "night" style, other styles are "storm" and "day"
+  style = "night",
+  -- disable italic for functions
+  styles = {
+    functions = "NONE"
+  },
+  sidebars = { "qf", "vista_kind", "terminal", "packer" },
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  on_colors = function(colors)
+    colors.hint = colors.orange
+    colors.error = "#ff0000"
+  end
+})
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
